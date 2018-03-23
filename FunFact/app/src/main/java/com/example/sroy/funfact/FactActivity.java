7af979cda13e0cff3189c0fd2eb7a80b2dcddd84
Sumit Roy <sroy8091@gmail.com>
2018-03-23 15:40:37 +0530

    online api added for cat facts and offline supprt also

--- FunFact/app/src/main/java/com/example/sroy/funfact/FactActivity.java
+++ FunFact/app/src/main/java/com/example/sroy/funfact/FactActivity.java
@@ -1,15 +1,33 @@
 package com.example.sroy.funfact;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Random;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class FactActivity extends AppCompatActivity {
     private FactBook factBook = new FactBook();
@@ -18,6 +36,9 @@ public class FactActivity extends AppCompatActivity {
     private TextView factTextView;
     private Button showFactButton;
     private RelativeLayout relativeLayout;
+    private ProgressBar progressBar;
+    // API variables
+    String API_URL = "https://fact.birb.pw/api/v1/cat";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +49,7 @@ public class FactActivity extends AppCompatActivity {
         factTextView = findViewById(R.id.factTextView);
         showFactButton = findViewById(R.id.showFactButton);
         relativeLayout = findViewById(R.id.relativeView);
+        progressBar = findViewById(R.id.progressBar);
 
         // Function of the button
         View.OnClickListener listener = new View.OnClickListener() {
@@ -35,15 +57,78 @@ public class FactActivity extends AppCompatActivity {
             public void onClick(View view) {
                 // The button is clicked so update the text view with new fact
 
-                String fact = factBook.getFact();
+                if(isNetworkAvailable()){
+                    new RetrieveFeedTask().execute();
+                }
+                else{
+                    factTextView.setText(factBook.getFact());
+                    Toast.makeText(FactActivity.this, "Pease connect with internet to get more cat facts", Toast.LENGTH_SHORT).show();
+                }
                 int colour = colorWheel.getColors();
-                // Update screen with new fact
-                relativeLayout.setBackgroundColor(colour);
-                factTextView.setText(fact);
                 showFactButton.setTextColor(colour);
+                relativeLayout.setBackgroundColor(colour);
             }
         };
         showFactButton.setOnClickListener(listener);
 
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        protected void onPreExecute() {
+            progressBar.setVisibility(View.VISIBLE);
+            factTextView.setText("");
+        }
+
+        protected String doInBackground(Void... urls) {
+//            String email = emailText.getText().toString();
+            // Do some validation here
+
+            try {
+                URL url = new URL(API_URL);
+                HttpsURLConnection urlConnection = (HttpsURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("FactActivityError", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            progressBar.setVisibility(View.GONE);
+//            Log.e("INFO", response);
+
+            try {
+                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+                String res = object.getString("string");
+                factTextView.setText(res);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- FunFact/app/src/main/java/com/example/sroy/funfact/FactBook.java
+++ FunFact/app/src/main/java/com/example/sroy/funfact/FactBook.java
@@ -10,16 +10,13 @@ class FactBook {
 
     // List of facts
     String[] facts = {
-            "Ants stretch when they wake up in the morning.",
-            "Ostriches can run faster than horses.",
-            "Olympic gold medals are actually made mostly of silver.",
-            "You are born with 300 bones; by the time you are an adult you will have 206.",
-            "It takes about 8 minutes for light from the Sun to reach Earth.",
-            "Some bamboo plants can grow almost a meter in just one day.",
-            "The state of Florida is bigger than England.",
-            "Some penguins can leap 2-3 meters out of the water.",
-            "On average, it takes 66 days to form a new habit.",
-            "Mammoths still walked the earth when the Great Pyramid was being built." };
+            "The average litter of kittens is between 2 - 6 kittens.",
+            "Unlike other cats, lions have a tuft of hair at the end of their tails.",
+            "Cats can’t see directly below their noses.",
+            "Cats (and dogs) sweat through their paws.",
+            "Domestic cats purr both when inhaling and when exhaling.",
+            "Cats are way more popular in America than dogs are (88 million > 74 million).",
+            "A cat's normal temperature varies around 101 degrees Fahrenheit.",};
 
     String getFact() {
 
